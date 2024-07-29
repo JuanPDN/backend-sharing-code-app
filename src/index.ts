ff6aabfc507ef6f398931593d46f746b3eea0b03
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-07-29 10:32:58 -0500

    feat(express): start server express

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+import { app } from "./app";
+import { connectToDatabase } from "./db/db";
+
+
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+connectToDatabase();
+
+app.listen(PORT, () => { 
+  console.log("Server running at PORT: ", PORT); 
+}).on("error", (error) => {
+  throw new Error(error.message);
+});
