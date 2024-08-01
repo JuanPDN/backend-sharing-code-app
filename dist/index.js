17b9cec9f6e5fe5bb9c8773b12fd35918a00c3b8
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-08-01 18:29:18 -0500

    deploy

--- dist/index.js
+++ dist/index.js
@@ -4,12 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
-const app_1 = require("./app");
+const app_1 = __importDefault(require("./app"));
 const db_1 = require("./db/db");
 dotenv_1.default.config();
 const PORT = process.env.PORT || 3000;
 (0, db_1.connectToDatabase)();
-app_1.app.listen(PORT, () => {
+app_1.default.listen(PORT, () => {
     console.log("Server running at PORT: ", PORT);
 }).on("error", (error) => {
     throw new Error(error.message);
