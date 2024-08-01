01a4f3db3577579fef6e38b8d4ce903c430049d0
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-08-01 16:14:56 -0500

    deploy

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,15 @@
+import dotenv from "dotenv";
 import { app } from "../src/app";
+import { connectToDatabase } from "../src/db/db";
 
-export default app
+
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+connectToDatabase();
+
+app.listen(PORT, () => { 
+  console.log("Server running at PORT: ", PORT); 
+}).on("error", (error) => {
+  throw new Error(error.message);
+});
--- dist/api/index.js
+++ dist/api/index.js
@@ -1,4 +1,16 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
 const app_1 = require("../src/app");
-exports.default = app_1.app;
+const db_1 = require("../src/db/db");
+dotenv_1.default.config();
+const PORT = process.env.PORT || 3000;
+(0, db_1.connectToDatabase)();
+app_1.app.listen(PORT, () => {
+    console.log("Server running at PORT: ", PORT);
+}).on("error", (error) => {
+    throw new Error(error.message);
+});
