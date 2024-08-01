28cc7f432821d009907add7670010a5ac30f1c3f
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-08-01 16:44:42 -0500

    deploy

--- api/index.ts
+++ api/index.ts
@@ -1,15 +1,3 @@
-import dotenv from "dotenv";
 import { app } from "../src/app";
-import { connectToDatabase } from "../src/db/db";
 
-
-dotenv.config();
-const PORT = process.env.PORT || 3000;
-
-connectToDatabase();
-
-app.listen(PORT, () => { 
-  console.log("Server running at PORT: ", PORT); 
-}).on("error", (error) => {
-  throw new Error(error.message);
-});
+export default app
