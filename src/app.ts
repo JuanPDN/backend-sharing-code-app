f3eb650fbf80fe70f2cfc909d96d172e705c12eb
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-08-01 18:28:30 -0500

    deploy

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,14 @@ import express from "express";
 import router from "./routes/routes";
 import cors from 'cors';
 
-export const app = express();
+const app = express();
 
 app.use(cors({
-    origin: '*',
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'] ,
 }))
 app.use(express.json())
 app.use('/api', router)
+
+export default app
